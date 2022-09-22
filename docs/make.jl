ea49715c85c8d536ac51dad0a88136ec91b1c9a1
C.Brenhin Keller <cbkeller@dartmouth.edu>
2022-09-22 11:27:24 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ docs/make.jl
@@ -0,0 +1,25 @@
+using MyPkg
+using Documenter
+
+DocMeta.setdocmeta!(MyPkg, :DocTestSetup, :(using MyPkg); recursive=true)
+
+makedocs(;
+    modules=[MyPkg],
+    authors="C. Brenhin Keller",
+    repo="https://github.com/brenhinkeller/MyPkg.jl/blob/{commit}{path}#{line}",
+    sitename="MyPkg.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://brenhinkeller.github.io/MyPkg.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/brenhinkeller/MyPkg.jl",
+    devbranch="main",
+)
